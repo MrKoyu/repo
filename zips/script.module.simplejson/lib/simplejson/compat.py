8119a699e3754d4cf52e4eef573d0bad1c192856
MrKoyu <40326868+MrKoyu@users.noreply.github.com>
2018-07-02 22:54:53 +0100

    .

--- zips/script.module.simplejson/lib/simplejson/compat.py
+++ zips/script.module.simplejson/lib/simplejson/compat.py
@@ -20,10 +20,7 @@ if sys.version_info[0] < 3:
 
 else:
     PY3 = True
-    if sys.version_info[:2] >= (3, 4):
-        from importlib import reload as reload_module
-    else:
-        from imp import reload as reload_module
+    from imp import reload as reload_module
     import codecs
     def b(s):
         return codecs.latin_1_encode(s)[0]
